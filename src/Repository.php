c55f95f7aee3e93f0c4e4cc5fb548ea5e5f80d55
RudyMasGoNext <rudy.mas@go-next.be>
2024-10-09 11:01:33 +0200

    Added method new()

--- src/Repository.php
+++ src/Repository.php
@@ -10,7 +10,7 @@ use Iterator;
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024, rudymas.be. (http://www.rudymas.be/)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 1.5.4
+ * @version 1.5.5
  * @lastmodified 2024-10-09
  * @package Tigress\Repository
  */
@@ -34,7 +34,7 @@ class Repository implements Iterator
      */
     public static function version(): string
     {
-        return '1.5.4';
+        return '1.5.5';
     }
 
     public function __construct()
@@ -139,9 +139,22 @@ class Repository implements Iterator
     }
 
     /**
+     * Load data from the database based on the query
+     *
+     * @return void
+     */
+    public function new(): void
+    {
+        $Model = 'Model\\' . $this->model;
+        $newModel = new $Model();
+        $newModel->initiateModel($this->fields);
+        $this->position = count($this->objects);
+        $this->objects[$this->position] = $newModel;
+    }
+
+    /**
      * Save the object
      *
-     * @param object $object
      * @return void
      */
     public function save(): void

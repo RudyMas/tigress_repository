da516b617fb0cc88c698feef03b9d3f890e6ed2e
RudyMasGoNext <rudy.mas@go-next.be>
2025-05-19 10:20:31 +0200

    Bugfix: I needed to use != instead of !== with function find & findFirst

--- src/Repository.php
+++ src/Repository.php
@@ -11,7 +11,7 @@ use Iterator;
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024-2025, rudymas.be. (http://www.rudymas.be/)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 2025.05.14.0
+ * @version 2025.05.19.0
  * @package Tigress\Repository
  */
 class Repository implements Iterator
@@ -34,7 +34,7 @@ class Repository implements Iterator
      */
     public static function version(): string
     {
-        return '2025.03.25';
+        return '2025.05.19';
     }
 
     public function __construct()
@@ -634,7 +634,7 @@ class Repository implements Iterator
         foreach ($this->objects as $object) {
             $found = true;
             foreach ($find as $key => $value) {
-                if ($object->$key !== $value) {
+                if ($object->$key != $value) {
                     $found = false;
                     break;
                 }
@@ -657,7 +657,7 @@ class Repository implements Iterator
         foreach ($this->objects as $object) {
             $found = true;
             foreach ($find as $key => $value) {
-                if ($object->$key !== $value) {
+                if ($object->$key != $value) {
                     $found = false;
                     break;
                 }

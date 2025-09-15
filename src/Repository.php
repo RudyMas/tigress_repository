54657b4e0450dc7c6266682cab05b7cfd852f68d
RudyMasGoNext <rudy.mas@go-next.be>
2025-09-15 14:47:16 +0200

    version

--- src/Repository.php
+++ src/Repository.php
@@ -11,7 +11,7 @@ use Iterator;
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024-2025, rudymas.be. (http://www.rudymas.be/)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 2025.09.15.0
+ * @version 2025.09.15.1
  * @package Tigress\Repository
  */
 class Repository implements Iterator

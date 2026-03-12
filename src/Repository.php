d04246ffa2eba678913d52920b8aeb73cfd04416
RudyMasGoNext <rudy.mas@go-next.be>
2026-03-12 14:17:54 +0100

    Added transaction compatability

--- src/Repository.php
+++ src/Repository.php
@@ -12,7 +12,7 @@ use Throwable;
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024-2026, rudymas.be. (http://www.rudymas.be/)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 2026.02.04.0
+ * @version 2026.03.12.0
  * @package Tigress\Repository
  */
 class Repository implements Iterator
@@ -36,7 +36,7 @@ class Repository implements Iterator
      */
     public static function version(): string
     {
-        return '2026.02.04';
+        return '2026.03.12';
     }
 
     /**
@@ -58,6 +58,19 @@ class Repository implements Iterator
         if (file_exists($file)) {
             TRANSLATIONS->load($file);
         }
+    }
+
+    /**
+     * Turns off autocommit mode. While autocommit mode is turned off, changes made to the database via the PDO object
+     * instance are not committed until you end the transaction by calling either the commit() method to commit the
+     * changes, or the rollBack() method to discard the changes. If you are in a transaction and you issue another call
+     * to beginTransaction(), PDO will issue an implicit commit of the current transaction before beginning a new one.
+     *
+     * @return bool
+     */
+    public function beginTransaction(): bool
+    {
+        return $this->database->beginTransaction();
     }
 
     /**
@@ -134,6 +147,16 @@ class Repository implements Iterator
         foreach ($seedSqls as $sql) {
             $this->safeSeed($sql);
         }
+    }
+
+    /**
+     * Commits a transaction, returning the database connection to autocommit mode.
+     *
+     * @return bool
+     */
+    public function commit(): bool
+    {
+        return $this->database->commit();
     }
 
     /**
@@ -809,6 +832,16 @@ class Repository implements Iterator
     public function rewind(): void
     {
         $this->position = 0;
+    }
+
+    /**
+     * Rolls back the current transaction, reverting the database connection to autocommit mode.
+     *
+     * @return bool
+     */
+    public function rollBack(): bool
+    {
+        return $this->database->rollBack();
     }
 
     /**
